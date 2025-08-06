955504979f57b4f02b9c077a471e50a00b7cdf11
achrafLIHEOUEL <liheouelachraf2001@gmail.com>
2025-08-06 16:05:17 +0100

    dashboard2

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,16 +14,35 @@ class HomeController extends AbstractController
         return $this->render('acceuil.html.twig');
     }
 
-    #[Route('/dashboard', name: 'app_dashboard')]
-    public function dashboard(): Response
-    {
-        return $this->render('dashboard.html.twig');
-    }
 
     #[Route('/profile', name: 'app_profile')]
     public function profile(): Response
     {
         return $this->render('utilisateur/profile.html.twig');
+    }
+
+    #[Route('/alerte', name: 'app_alerte')]
+    public function alerte(): Response
+    {
+        return $this->render('alerte/listeAlerte.html.twig');
+    }
+
+    #[Route('/intervention', name: 'app_intervention')]
+    public function intervention(): Response
+    {
+        return $this->render('intervention/listeIntervention.html.twig');
+    }
+
+    #[Route('/affectation', name: 'app_affectation')]
+    public function affectation(): Response
+    {
+        return $this->render('affectation/listeAffectation.html.twig');
+    }
+
+    #[Route('/reclamation', name: 'app_reclamation')]
+    public function reclamation(): Response
+    {
+        return $this->render('reclamation/listeReclamation.html.twig');
     }
 }
 
